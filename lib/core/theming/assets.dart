00bd198deae1a306106d258a465b49a99742fa2d
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-02-23 07:56:22 +0200

    create splah view

--- lib/core/theming/assets.dart
+++ lib/core/theming/assets.dart
@@ -2,4 +2,5 @@ class AppAssets {
   static const String logo = 'assets/svgs/logo.svg';
   static const String logoLowOpacity = 'assets/svgs/logo_low_opacity.svg';
   static const String onboardingDoctor = "assets/images/onboarding_doctor.png";
+  static const String splash = "assets/images/splash.png";
 }
